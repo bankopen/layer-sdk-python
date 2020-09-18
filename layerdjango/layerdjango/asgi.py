460cc77c9077a856b8fe5a273eb72f9b78b6ebaf
viatechs <support@viatechs.in>
2020-09-18 16:43:42 +0530

    Add files via upload

---
+++ layerdjango/layerdjango/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for layerdjango project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'layerdjango.settings')
+
+application = get_asgi_application()
